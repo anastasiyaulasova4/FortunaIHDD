4146cfda414d3a48b4f18fb02ecbca8a7107d991
Anastasiya Ulasova <anastasiyaulasova4@gmail.com>
2020-12-13 13:25:12 +0800

    Suiting new name

--- change.sh
+++ change.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i 's/="assets\//="https:\/\/cdn.jsdelivr.net\/gh\/anastasiyaulasova4\/FortunaIHDD\/src\/assets\//g' ./*.html
-sed -i 's/="images\//="https:\/\/cdn.jsdelivr.net\/gh\/anastasiyaulasova4\/FortunaIHDD\/src\/images\//g' ./*.html
-sed -i 's/="videos\//="https:\/\/cdn.jsdelivr.net\/gh\/anastasiyaulasova4\/FortunaIHDD\/src\/videos\//g' ./*.html
+sed -i 's/="assets\//="https:\/\/cdn.jsdelivr.net\/gh\/anastasiyaulasova4\/fortuna-ihdd\/src\/assets\//g' ./*.html
+sed -i 's/="images\//="https:\/\/cdn.jsdelivr.net\/gh\/anastasiyaulasova4\/fortuna-ihdd\/src\/images\//g' ./*.html
+sed -i 's/="videos\//="https:\/\/cdn.jsdelivr.net\/gh\/anastasiyaulasova4\/fortuna-ihdd\/src\/videos\//g' ./*.html
